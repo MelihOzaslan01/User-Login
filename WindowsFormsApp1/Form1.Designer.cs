2979728ce2d1b320a94b692d7b5eee2c581e1ea6
Melih Özaslan <melihozaslan01@gmail.com>
2022-05-03 20:07:09 +0300

    User Login

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TabLoginBox = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.TxtMail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.TxtUserPassword = new System.Windows.Forms.TextBox();
+            this.TxtUserName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.TabLoginBox.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TabLoginBox
+            // 
+            this.TabLoginBox.Controls.Add(this.tabPage1);
+            this.TabLoginBox.Controls.Add(this.tabPage2);
+            this.TabLoginBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TabLoginBox.HotTrack = true;
+            this.TabLoginBox.Location = new System.Drawing.Point(0, 0);
+            this.TabLoginBox.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.TabLoginBox.Name = "TabLoginBox";
+            this.TabLoginBox.SelectedIndex = 0;
+            this.TabLoginBox.Size = new System.Drawing.Size(376, 186);
+            this.TabLoginBox.TabIndex = 1;
+            this.TabLoginBox.SelectedIndexChanged += new System.EventHandler(this.TabLoginBox_SelectedIndexChanged);
+            this.TabLoginBox.TabIndexChanged += new System.EventHandler(this.TabLoginBox_TabIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage1.Controls.Add(this.TxtPassword);
+            this.tabPage1.Controls.Add(this.TxtMail);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.tabPage1.Size = new System.Drawing.Size(368, 160);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Mail ile giriş";
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.1F);
+            this.TxtPassword.Location = new System.Drawing.Point(151, 50);
+            this.TxtPassword.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.PasswordChar = '*';
+            this.TxtPassword.Size = new System.Drawing.Size(126, 26);
+            this.TxtPassword.TabIndex = 3;
+            // 
+            // TxtMail
+            // 
+            this.TxtMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.1F);
+            this.TxtMail.Location = new System.Drawing.Point(151, 22);
+            this.TxtMail.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.TxtMail.Name = "TxtMail";
+            this.TxtMail.Size = new System.Drawing.Size(126, 26);
+            this.TxtMail.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.1F);
+            this.label2.Location = new System.Drawing.Point(16, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.1F);
+            this.label1.Location = new System.Drawing.Point(16, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mail adresi:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.RosyBrown;
+            this.tabPage2.Controls.Add(this.TxtUserPassword);
+            this.tabPage2.Controls.Add(this.TxtUserName);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.tabPage2.Size = new System.Drawing.Size(368, 160);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Kullanıcı Adı ile Giriş";
+            // 
+            // TxtUserPassword
+            // 
+            this.TxtUserPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.1F);
+            this.TxtUserPassword.Location = new System.Drawing.Point(144, 55);
+            this.TxtUserPassword.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.TxtUserPassword.Name = "TxtUserPassword";
+            this.TxtUserPassword.PasswordChar = '*';
+            this.TxtUserPassword.Size = new System.Drawing.Size(126, 26);
+            this.TxtUserPassword.TabIndex = 7;
+            // 
+            // TxtUserName
+            // 
+            this.TxtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.1F);
+            this.TxtUserName.Location = new System.Drawing.Point(144, 26);
+            this.TxtUserName.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.TxtUserName.Name = "TxtUserName";
+            this.TxtUserName.Size = new System.Drawing.Size(126, 26);
+            this.TxtUserName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.1F);
+            this.label3.Location = new System.Drawing.Point(10, 57);
+            this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Şifre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.1F);
+            this.label4.Location = new System.Drawing.Point(10, 26);
+            this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Kullanıcı adı:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.BtnExit);
+            this.panel1.Controls.Add(this.BtnLogin);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 124);
+            this.panel1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(376, 62);
+            this.panel1.TabIndex = 1;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(266, 9);
+            this.BtnExit.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(87, 47);
+            this.BtnExit.TabIndex = 1;
+            this.BtnExit.Text = "Çıkış";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Location = new System.Drawing.Point(10, 9);
+            this.BtnLogin.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(108, 47);
+            this.BtnLogin.TabIndex = 0;
+            this.BtnLogin.Text = "Giriş";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 186);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.TabLoginBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Ekranı";
+            this.TabLoginBox.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl TabLoginBox;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.TextBox TxtMail;
+        private System.Windows.Forms.TextBox TxtUserPassword;
+        private System.Windows.Forms.TextBox TxtUserName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Button BtnLogin;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FrmLogin : Form
+    {
+        private enum TLoginType { LtMail, LtUsername };
+        private TLoginType LoginType = TLoginType.LtMail;
+
+        private const string DbMailAddress = "mail";
+        private const string DbPassword1 = "password";
+
+        private TLoginType LoginType2 = TLoginType.LtUsername;
+        private const string DbUsername = "admin";
+        private const string DbPassword2 = "password";
+
+
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string Username, Password, MailAddress;
+
+            if (LoginType == TLoginType.LtMail)
+            {
+
+                if (TxtMail.Text.Trim() == "" || TxtPassword.Text.Trim() == "")
+                    MessageBox.Show("Mail adresi veya şifre boş olamaz.");
+                else
+                {
+                    MailAddress = TxtMail.Text.Trim();
+                    Password = TxtPassword.Text.Trim();
+
+                    if (DbMailAddress != MailAddress)
+                        MessageBox.Show("Giriş bilgileri hatalı");
+                    else
+                    {
+                        if (DbPassword1 != Password)
+                            MessageBox.Show("Giriş bilgileri hatalı");
+                        else
+                        {
+                            if (DbMailAddress == MailAddress && DbPassword1 == Password)
+                                MessageBox.Show("Giriş başarılı");
+                        }
+                    }
+
+
+
+                }
+
+            }
+
+            if (LoginType2 == TLoginType.LtUsername)
+            {
+                if (TxtUserName.Text.Trim() == "" || TxtUserPassword.Text.Trim() == "")
+                    MessageBox.Show("Kullanıcı Adı veya Şifresi Boş Olamaz...");
+                else
+                {
+                    Username = TxtUserName.Text.Trim();
+                    Password= TxtUserPassword.Text.Trim();
+
+                    if (DbUsername != Username)
+                        MessageBox.Show("Giriş bilgileri hatalı");
+                    else
+                    {
+                        if (DbPassword2 != Password)
+                            MessageBox.Show("Giriş bilgileri hatalı");
+                        else
+                        {
+                            if(DbUsername == Username && DbPassword2 == Password)
+                                MessageBox.Show("Tebrikler Giriş Başarılı");
+                        }
+
+
+                    }
+
+
+
+
+                }
+            }
+
+        }
+        private void TabLoginBox_TabIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TabLoginBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (TabLoginBox.SelectedIndex == 0)
+                LoginType = TLoginType.LtMail;
+            else if (TabLoginBox.SelectedIndex == 1)
+                LoginType = TLoginType.LtUsername;
+        }
+    }
+}
